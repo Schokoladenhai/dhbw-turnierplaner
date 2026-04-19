aec59625e163a3818f5bd7572cf3b593a9086f98
Schokoladenhai <preiss05@gmx.de>
2026-04-19 12:44:56 +0200

    feat(backend) eine UUID Funktion erstellt

---
+++ backend/src/uuid-gen.cpp
@@ -0,0 +1,19 @@
+#include "uuid-gen.hpp"
+#include <random>
+#include <algorithm>
+#include <array>
+#include "uuid.h"
+
+uuids::uuid generateUuid() {
+    static std::mt19937 generator = []() {
+        std::random_device rd;
+        std::array<int, std::mt19937::state_size> seed_data;
+        std::generate(seed_data.begin(), seed_data.end(), std::ref(rd));
+        std::seed_seq seq(seed_data.begin(), seed_data.end());
+        return std::mt19937(seq);
+    }();
+
+    static uuids::uuid_random_generator gen(&generator);
+
+    return gen();
+}
