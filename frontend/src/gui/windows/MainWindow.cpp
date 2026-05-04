6c022c12726a0420731fde3ee7966999eaa8ad50
Theo <es24034@lehre.dhbw-stuttgart.de>
2026-05-04 11:04:58 +0200

    push for main test

--- frontend/src/gui/windows/MainWindow.cpp
+++ frontend/src/gui/windows/MainWindow.cpp
@@ -1,5 +1,6 @@
 #include "gui/windows/MainWindow.hpp"
 #include "ui_MainWindow.h"
+#include "classes/match.hpp"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -10,8 +11,19 @@ MainWindow::MainWindow(QWidget *parent) :
     // Wir erstellen ein vertikales Layout im zentralen Bereich des Fensters
     m_contentLayout = new QVBoxLayout(ui->centralwidget);
 
+    Match match1("Match 1", uuids::uuid{});
+    Team teamA("Team A", uuids::uuid{});
+    Team teamB("Team B", uuids::uuid{});
+    match1.addTeam(teamA);
+    match1.addTeam(teamB);
+    MatchWidget* matchWidget1 = new MatchWidget(this);
+    matchWidget1->setTeams(
+    QString::fromStdString(teamA.getName()), 
+    QString::fromStdString(teamB.getName())
+    );
+    m_contentLayout->addWidget(matchWidget1);
     // Testweise ein paar MatchWidgets hinzufügen
-    for(int i = 0; i < 3; ++i) {
+    for(int i = 0; i < 10; ++i) {
         MatchWidget* match = new MatchWidget(this);
         match->setTeams("Team " + QString::number(i*2+1), "Team " + QString::number(i*2+2));
         
